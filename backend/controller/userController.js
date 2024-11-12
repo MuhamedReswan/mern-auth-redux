7d385e61bae8d5220c167f40e1431fe08025431f
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-11-12 14:53:51 +0530

    completed all routing

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,5 +1,29 @@
-const authUser = (req, res) => {
-  res.status(200).json({ message: "Auth User" });
-};
+import asyncHandler from "express-async-handler";
 
-export { authUser };
+const authUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Auth User" });
+});
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update user Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
