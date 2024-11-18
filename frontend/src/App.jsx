7f1fddd9d0612a684946167deaab8ad1ce2232bd
MuhamedReswan <muhamedreswan9917@gmail.com>
2024-11-18 18:05:03 +0530

    UI started and created basic Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+import Header from "./components/Header.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-yellow-500">
+        <Header />
+        <HomeScreen />
+      </div>
+    </>
+  );
+};
+
+export default App;
